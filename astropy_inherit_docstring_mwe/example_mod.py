888e41076f06bf1f9d83e4fca1855cbcab6d5d7d
Michael S. P. Kelley <msk@astro.umd.edu>
2024-07-24 20:00:41 -0400

    Update with a different fix.

--- astropy_inherit_docstring_mwe/example_mod.py
+++ astropy_inherit_docstring_mwe/example_mod.py
@@ -7,8 +7,10 @@ class ArbitraryFrame(BaseCoordinateFrame):
 # comment these fixes out and build_docs will fail with
 # /disks/data0/astro/Projects/astropy-inherit-docstring-mwe/.tox/build_docs/lib/python3.11/site-packages/astropy_inherit_docstring_mwe/example_mod.py:docstring of astropy.coordinates.baseframe.BaseCoordinateFrame.separation_3d:4: WARNING: unknown document: 'astropy:/coordinates/matchsep'
 ArbitraryFrame.separation.__doc__ = ArbitraryFrame.separation.__doc__.replace(
-    ":doc:`astropy:/", ":doc:`astropy:"
+    ":doc:`astropy:/coordinates/matchsep`",
+    ":ref:`astropy-coordinates-separations-matching`",
 )
 ArbitraryFrame.separation_3d.__doc__ = ArbitraryFrame.separation_3d.__doc__.replace(
-    ":doc:`astropy:/", ":doc:`astropy:"
+    ":doc:`astropy:/coordinates/matchsep`",
+    ":ref:`astropy-coordinates-separations-matching`",
 )
